4376d62b40d815598b067664ac5e7ea9e36f0365
mhmscpo <136313603+mhmscpo@users.noreply.github.com>
2023-06-12 13:49:32 +0100

    Utilisation de la props par défaut children

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -5,7 +5,8 @@ import Button from './components/ui/Button/Button'
 function App() {
   return (
     <div className="App">
-      <Button></Button>
+        <Button><img src='/img/9165532_ok_hand_icon.png' alt='Ok'/></Button>
+        <Button text="Cancel"></Button>
     </div>
   );
 }
--- src/App/components/ui/Button/Button.jsx
+++ src/App/components/ui/Button/Button.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import style from './Button.module.css'
 
 const Button=(props)=> {
-    return <button className={style.Button}>Benjamin</button>;
+    console.log(props);
+    return (
+        <button className={style.Button}>
+            {undefined !== props.children ? props.children : props.text}
+        </button>
+    );
 }
 export default Button
