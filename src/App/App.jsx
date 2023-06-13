74dd2dee51b76c5688cb50c98a199911fe2d51ad
mhmscpo <136313603+mhmscpo@users.noreply.github.com>
2023-06-13 08:17:32 +0100

    Exercice changt etat bouton avec timeout, useEffect

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Button from './components/ui/Button/Button'
 
 function App() {
+  const [counter, setcounter] = useState(0)
+
+  useEffect(()=>{
+      console.log('Valeur post setCounter', counter)
+      return() => {
+        //console.log('cleanup') //effect
+      }
+  }, [counter])
+
+  useEffect(() => {
+      setcounter(1)
+  }, [])
+
+  console.log('rendu');
   return (
     <div className="App">
-        <Button onClick={(unParamDeLEnfant)=>{
-            console.log('fonction evnoyee par le parent', unParamDeLEnfant)
-        }}><img src='/img/9165532_ok_hand_icon.png' alt='Ok'/></Button>
-        <Button className="error">Cancel</Button>
+      Voici la valeur de counter : {counter}
+      <hr/>
+      <Button className="error" onClick={()=>{
+        setcounter(counter-1)
+        console.log(counter)
+      }}>-1</Button>
+      <Button onClick={()=>{
+        setcounter(counter+1)
+        console.log(counter)
+      }}>+1</Button>
     </div>
   );
 }
--- src/App/components/ui/Button/Button.jsx
+++ src/App/components/ui/Button/Button.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import style from './Button.module.css'
 import PropTypes from 'prop-types'
+import { cleanup } from '@testing-library/react'
 
 const Button=(props)=> {
+
+    const [isClicked, setIsClicked ] = useState(false)
+
+    useEffect(() => {
+        if (isClicked) {
+            setTimeout(()=>{setIsClicked(false)}, 500 )
+        }
+        return ()=> {
+            clearTimeout()
+        }
+    }, [isClicked])
+
     console.log(props);
     return (
         <button 
             type={props.type} 
-            className={`${style.Button} btn${undefined!==props.className? ' '+props.className:''}`} 
+            className={`${style.Button} btn${undefined!==props.className? ' '+props.className:''}${isClicked===true ? ' '+style.clicked : ' '}`} 
             onClick={(evt)=>{
                 if ( undefined !== props.onClick && typeof props.onClick === 'function' ) {
+                    setIsClicked(true);
                     props.onClick('click');
                 }
             }}>
