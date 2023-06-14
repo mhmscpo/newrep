2d20ed3a06f44b439c1037d8a5647a8c10242f2e
michel.howardmaurice@sciencespo.fr <michel.howardmaurice@sciencespo.fr>
2023-06-14 10:00:55 +0100

    Fichier de configuration des Ressources

---
+++ src/App/config/config.js
@@ -0,0 +1,11 @@
+/**
+ * Base d'URL du serveur REST
+ */
+export const REST_ADR="http://localhoste:5679"
+/**
+ * URI des ressources en focntion du nom de ressource
+ */
+export const ressourceURI=Object.freeze({
+    memes:'/memes',
+    images: '/images'
+});
